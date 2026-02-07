f99442816e92a734be95cd0b72fda30045f1cd5b
armenvart777 <armvart00.00@gmail.com>
2026-02-07 14:23:00 +0300

    fix scheduler interval

--- scheduler.py
+++ scheduler.py
@@ -4,6 +4,8 @@ from services.subscription import check_expirations
 
 
 async def run_scheduler(bot: Bot):
+    # Check every 60 minutes
+    interval = 3600
     while True:
         await check_expirations(bot)
-        await asyncio.sleep(3600)
+        await asyncio.sleep(interval)
